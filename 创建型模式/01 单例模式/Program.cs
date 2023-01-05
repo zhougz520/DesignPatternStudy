41f89fac8517dd2ffaec4990992322b06416c88d
zhougz520 <zhougz520@gmail.com>
2023-01-05 11:03:57 +0800

    chore: update 单例模式，增加多线程时的处理。 分为“懒汉模式” & “饿汉模式”

--- 创建型模式/01 单例模式/Program.cs
+++ 创建型模式/01 单例模式/Program.cs
@@ -1,8 +1,24 @@
-﻿Singleton s1 = Singleton.GetInstance();
-Singleton s2 = Singleton.GetInstance();
+﻿/*
+    客户端代码（懒汉模式）
+*/
+SingletonLazy sl1 = SingletonLazy.GetInstance();
+SingletonLazy sl2 = SingletonLazy.GetInstance();
 
 // 比较两次实例化后对象的结果是实例相同
-if (s1 == s2)
+if (sl1 == sl2)
 {
-    Console.WriteLine("两个对象是相同实例。");
+    Console.WriteLine("懒汉模式：两个对象是相同实例。");
+}
+
+
+/*
+    客户端代码（饿汉模式）
+*/
+SingletonHungry sh1 = SingletonHungry.GetInstance();
+SingletonHungry sh2 = SingletonHungry.GetInstance();
+
+// 比较两次实例化后对象的结果是实例相同
+if (sh1 == sh2)
+{
+    Console.WriteLine("饿汉模式：两个对象是相同实例。");
 }
--- 创建型模式/01 单例模式/src/Singleton.cs
+++ 创建型模式/01 单例模式/src/Singleton.cs
@@ -1,18 +1,51 @@
-public class Singleton
+/*
+    懒汉模式：
+    sealed阻止发生派生，而派生可能会增加实例
+*/
+public sealed class SingletonLazy
 {
     // 定义变量用于存储单例的实例
-    private static Singleton? instance;
+    // 初始化时就实例化（懒汉模式）
+    private static readonly SingletonLazy instance = new SingletonLazy();
 
     // 构造方法让其private，避免外界利用new创建此类实例
-    private Singleton() { }
+    private SingletonLazy() { }
 
     // 此方法是获得本类实例的唯一全局访问点
-    public static Singleton GetInstance()
+    public static SingletonLazy GetInstance()
     {
-        // 若实例不存在，则new一个新实例，否则返回已有的实例
+        return instance;
+    }
+}
+
+/*
+    饿汉模式：
+    通过“双重锁定”保证线程安全
+*/
+public class SingletonHungry
+{
+    // 定义变量用于存储单例的实例
+    private static SingletonHungry? instance;
+    // 程序运行时创建一个静态只读的进程辅助对象
+    private static readonly object syncRoot = new object();
+
+    // 构造方法让其private，避免外界利用new创建此类实例
+    private SingletonHungry() { }
+
+    // 此方法是获得本类实例的唯一全局访问点
+    public static SingletonHungry GetInstance()
+    {
+        // 先判断实例是否存在，不存在再加锁处理
         if (instance == null)
         {
-            instance = new Singleton();
+            // 在同一个时刻加了锁的那部分程序只有一个线程可以进入
+            lock (syncRoot)
+            {
+                if (instance == null)
+                {
+                    instance = new SingletonHungry();
+                }
+            }
         }
 
         return instance;
